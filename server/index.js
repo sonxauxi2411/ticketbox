9b1f78ee53e7c36662b48ca04b0bc68283939b62
sonxauxi2411 <sontnfx18388@funix.edu.vn>
2024-02-16 10:42:50 +0700

    chore: test-git

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}));
 app.use(session({
-  secret: 'ticketbox', 
+  secret: 'ticketbox-123', 
   resave: false,
   saveUninitialized: false,
 }));
